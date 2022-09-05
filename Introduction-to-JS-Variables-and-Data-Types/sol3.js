b56245510b2c8fdb52a95ebc69b8ab4f884258a1
MrVivek30 <vivekchaudhary538@gmail.com>
2022-09-06 00:23:29 +0530

    add

---
+++ Introduction-to-JS-Variables-and-Data-Types/sol3.js
@@ -0,0 +1,10 @@
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let Name="Vivek Chaudhary";
+let Age=21;
+console.log(Name);
+console.log(Age);
+console.log(typeof(Name));
+console.log(typeof(Age));
---
+++ Introduction-to-JS-Variables-and-Data-Types/sol4.js
@@ -0,0 +1,23 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+
+
+let Result="######R̳e̳p̳o̳r̳t̳ C̳a̳r̳d̳######"
+let Name="Vɪᴠᴇᴋ Cʜᴀᴜᴅʜᴀʀʏ";
+let School="*******   🆂🅷🆁🅴🅴 🅹🅰🅸🅽 🆅🅸🅳🅰🅻🅰🆈🅰   *******";
+let Grade="A";
+let Section="B";
+let Rollno="48";
+let DigitalSystemDesign="65";
+let ElectronicsDevice="70";
+let IntegratedCircuit="69";
+console.log(Result);
+console.log(Name);
+console.log(School);
+console.log(Grade);
+console.log(Section);
+console.log(Rollno);
+console.log(DigitalSystemDesign);
+console.log(ElectronicsDevice);
+console.log(IntegratedCircuit);
